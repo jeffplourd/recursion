5cc28a22c6c9e7d96ea271a31001b5833ec6cc87
jplourd <jeffplourd@gmail.com>
2015-05-29 13:27:22 -0700

    Added recursion method to deal with all data types besides standard objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	//I need to figure out a way to convert each data type to a string
+	//To convert Array's and Object's to strings, I will need to loop over them and use a recursive function
+	//for each element in the array or object.
+	if(Array.isArray(obj)) {
+		if(obj.length === 0) {
+			return '[]';
+		}else if (obj.length === 1 && typeof obj[0] === 'number') {
+			return '[' + obj[0] + ']';
+		}else if (obj.length === 1 && typeof obj[0] === 'string') {
+			return '[' + stringifyJSON(obj[0]) + ']';
+		}else {
+			
+			var accum = '';
+			_.each(obj, function(elem, index, collection) {
+				accum += stringifyJSON(elem) + ',';
+			});
+			return '[' + accum.slice(0,-1) + ']';
+		}
+		
+	}else if(typeof obj === "string") {
+		return '"' + obj + '"';
+	}else {
+		return String(obj);	
+	}
+
 };
