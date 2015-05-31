6dd6eea40b63d0520086539f62a33e21d9b180ed
jplourd <jeffplourd@gmail.com>
2015-05-31 14:19:08 -0700

    Added 'getElementByClass' that traverses the DOM and returns an array of nodes with the specified class.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var nodesWithClass = [];
+
+	function traverseTheDOM(node, func) {
+		func(node);
+		node = node.firstChild;
+		while(node) {
+			traverseTheDOM(node, func);
+			node = node.nextSibling;
+		}
+	}
+
+	function checkForClass(node) {
+		var hasClass = node.nodeType === 1 && node.classList.contains(className);
+		if(hasClass) {
+			nodesWithClass.push(node);
+		}
+	}
+
+	traverseTheDOM(document.body, checkForClass);
+
+	return nodesWithClass;
 };
